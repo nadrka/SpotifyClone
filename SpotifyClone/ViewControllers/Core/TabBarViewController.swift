d56914537a1f22b6fb58f8051245ae8bc80c32c9
Karol <nadratowskikarol@gmail.com>
2021-02-27 17:11:52 +0100

    Setup project structure

---
+++ SpotifyClone/ViewControllers/Core/TabBarViewController.swift
@@ -0,0 +1,22 @@
+//
+//  TabViewController.swift
+//  SpotifyClone
+//
+//  Created by karol.nadratowski on 27/02/2021.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        prepareViewControllers()
+    }
+    
+    private func prepareViewControllers() {
+        
+    }
+}
+
+
